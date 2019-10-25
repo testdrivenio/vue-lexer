df8f760cafdaf18c4a89ebbe6fd2425bba641c43
Michael Herman <hermanmu@gmail.com>
2019-10-25 10:18:02 -0600

    add black, flake8, and isort

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 test:
 	coverage run --source=vue setup.py test
+	flake8 ./vue --ignore=E501
+	flake8 ./tests --ignore=E501
+	isort ./vue/*.py --check-only
+	isort ./tests/*.py --check-only
+	black ./vue --check
+	black ./tests --check
 
 release:
 	rm -rf build dist
--- vue/lexer.py
+++ vue/lexer.py
@@ -1,58 +1,73 @@
 import re
 
-from pygments.lexer import bygroups, include, default
+from pygments.lexer import bygroups, default, include
 from pygments.lexers.javascript import JavascriptLexer
-from pygments.token import Name, Operator, Punctuation, String, Text, Literal
-
+from pygments.token import Literal, Name, Operator, Punctuation, String, Text
 
 # Use same tokens as `JavascriptLexer`, but with tags and attributes support
 TOKENS = JavascriptLexer.tokens
 
-TOKENS.update({
-    'vue': [
-        (r'(<)([\w]+)', bygroups(Punctuation, Name.Tag), 'tag'),
-        (r'(<)(/)([\w]+)(>)', bygroups(Punctuation, Punctuation, Name.Tag, Punctuation)),
-    ],
-    'tag': [
-        (r'\s+', Text),
-        (r'(-)([\w]+)', Name.Tag),
-        (r'(@[\w]+)(="[\S]+")(>)', bygroups(Name.Tag, Literal.String, Punctuation)),
-        (r'(@[\w]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
-        (r'(@[\S]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
-        (r'(:[\S]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
-        (r'(:)', Operator),
-        (r'(v-b-[\S]+)', Name.Tag),
-        (r'(v-[\w]+)(=".+)([:][\w]+)(="[\w]+")(>)', bygroups(Name.Tag, Literal.String, Name.Tag, Literal.String, Punctuation)),
-        (r'(v-[\w]+)(="[\S]+")(>)', bygroups(Name.Tag, Literal.String, Punctuation)),
-        (r'(v-[\w]+)(>)', bygroups(Name.Tag, Punctuation)),
-        (r'(v-[\w]+)(=".+")(>)', bygroups(Name.Tag, Literal.String, Punctuation)),
-        (r'(<)([\w]+)', bygroups(Punctuation, Name.Tag)),
-        (r'(<)(/)([\w]+)(>)', bygroups(Punctuation, Punctuation, Name.Tag, Punctuation)),
-        (r'([\w]+\s*)(=)(\s*)', bygroups(Name.Attribute, Operator, Text), 'attr'),
-        (r'[{}]+', Punctuation),
-        (r'[\w\.]+', Name.Attribute),
-        (r'(/?)(\s*)(>)', bygroups(Punctuation, Text, Punctuation), '#pop'),
-    ],
-    'attr': [
-        ('{', Punctuation, 'expression'),
-        ('".*?"', String, '#pop'),
-        ("'.*?'", String, '#pop'),
-        default ('#pop')
-    ],
-    'expression': [
-        ('{', Punctuation, '#push'),
-        ('}', Punctuation, '#pop'),
-        include('root')
-    ]
-})
-TOKENS['root'].insert(0, include('vue'))
+TOKENS.update(
+    {
+        "vue": [
+            (r"(<)([\w]+)", bygroups(Punctuation, Name.Tag), "tag"),
+            (
+                r"(<)(/)([\w]+)(>)",
+                bygroups(Punctuation, Punctuation, Name.Tag, Punctuation),
+            ),
+        ],
+        "tag": [
+            (r"\s+", Text),
+            (r"(-)([\w]+)", Name.Tag),
+            (r'(@[\w]+)(="[\S]+")(>)', bygroups(Name.Tag, Literal.String, Punctuation)),
+            (r'(@[\w]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
+            (r'(@[\S]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
+            (r'(:[\S]+)(="[\S]+")', bygroups(Name.Tag, Literal.String)),
+            (r"(:)", Operator),
+            (r"(v-b-[\S]+)", Name.Tag),
+            (
+                r'(v-[\w]+)(=".+)([:][\w]+)(="[\w]+")(>)',
+                bygroups(
+                    Name.Tag, Literal.String, Name.Tag, Literal.String, Punctuation
+                ),
+            ),
+            (
+                r'(v-[\w]+)(="[\S]+")(>)',
+                bygroups(Name.Tag, Literal.String, Punctuation),
+            ),
+            (r"(v-[\w]+)(>)", bygroups(Name.Tag, Punctuation)),
+            (r'(v-[\w]+)(=".+")(>)', bygroups(Name.Tag, Literal.String, Punctuation)),
+            (r"(<)([\w]+)", bygroups(Punctuation, Name.Tag)),
+            (
+                r"(<)(/)([\w]+)(>)",
+                bygroups(Punctuation, Punctuation, Name.Tag, Punctuation),
+            ),
+            (r"([\w]+\s*)(=)(\s*)", bygroups(Name.Attribute, Operator, Text), "attr"),
+            (r"[{}]+", Punctuation),
+            (r"[\w\.]+", Name.Attribute),
+            (r"(/?)(\s*)(>)", bygroups(Punctuation, Text, Punctuation), "#pop"),
+        ],
+        "attr": [
+            ("{", Punctuation, "expression"),
+            ('".*?"', String, "#pop"),
+            ("'.*?'", String, "#pop"),
+            default("#pop"),
+        ],
+        "expression": [
+            ("{", Punctuation, "#push"),
+            ("}", Punctuation, "#pop"),
+            include("root"),
+        ],
+    }
+)
+TOKENS["root"].insert(0, include("vue"))
 
 
 class VueLexer(JavascriptLexer):
-    name = 'vue'
-    aliases = ['vue', 'vuejs']
-    filenames = ['*.vue']
-    mimetypes = ['text/x-vue', 'application/x-vue']
+    name = "vue"
+    aliases = ["vue", "vuejs"]
+    filenames = ["*.vue"]
+    mimetypes = ["text/x-vue", "application/x-vue"]
 
     flags = re.MULTILINE | re.DOTALL | re.UNICODE
 
